8b12566751c9fc18616c64351d1c4d81d61d57cc
nikamgeliashvili <nmgeliashvili@ia.local>
2019-07-22 11:28:19 +0400

    new

---
+++ main.js
@@ -0,0 +1,854 @@
+// var namea = 20 
+
+// if ( namea % 2 == 0) {
+//     console.log( "ricxvi luwia")
+// }
+
+
+
+
+// for ( i = 30; i <= 60; i ++  ) {
+//     if ( i % 2 ==0 && i % 5 ==0 )
+    
+//     {
+//         console.log ( i )
+
+//      }
+
+// }
+
+
+
+
+// var jami = 0;
+
+// for ( i = 0;  i <= 10; i ++  ){
+//     jami = jami + i
+// } 
+
+// console.log( jami )
+
+
+
+
+
+// var namea = 5 
+
+// for ( i=20; i>0; i -- ){
+  
+//     if( i == namea ) { 
+//         alert( 'es cipria ' +i )
+
+//         break
+//     }console.log(i)
+// }
+
+
+// var jami=0
+// for (var i = 0; i < 20; i++) {
+
+//     if( i % 2 ==0  ){
+//         jami = jami + i
+//     }
+//     console.log(jami)
+    
+// }
+
+
+
+// var index = prompt("sheiyvanet index")
+
+// if( index % 2 ==0 && index% 5 !==0  ) {
+//     console.log("true")
+// }
+// else(
+
+// console.log("false")
+
+// )
+
+// var index = prompt('sheiyvanet dabadebis weli');
+
+// var age= 2019 - index
+
+// console.log(" pensiamde darcha "  + ( 60- age ) +  ' weli ')
+
+
+// var index = prompt('sheiyvanet asaki')
+
+// if( index <= 18){
+//     console.log("teenager")
+    
+// }
+
+// if( index => 18, index <= 60 ){
+//     console.log("shuaxnis")
+// }
+
+// if( index > 60 ){
+//     console.log("moxuci")
+// }
+
+
+
+// var index =  prompt ('sheiyvanet dabadebis weli')
+
+// var age = 2019 - index
+
+// if ( age % 2 ==0){
+//     console.log( age + ' luwi ')
+    
+// }
+
+// else{
+//     console.log( age + ' kenti ')
+// }
+
+// var index = 0
+// for ( var i = 0;  i <= 100;  i ++)
+
+// if( i % 5 == 0){
+//     document.write( i+ "  ")
+// }
+
+
+// var country = ["saqartvelo", 'somxeti', 'ruseti' ,'turqeti' ,'germania'];
+
+// var population = [1000, 2000, 3000, 4000, 5000];
+
+
+// for (var i = 0; i < country.length; i++){
+// if ( population[i] > 3000 )
+// {
+
+   
+//   console.log( population[i] + country[i])
+    
+// }
+
+
+// }
+
+
+// var index = prompt ( ' sheiyvanet dabadebis tarigi ' );
+
+// var age = 2019 - index; 
+
+// if  ( age > 17 && age < 31  ) 
+//  {   
+//      if ( age % 2 == 0 ){
+//          console.log(' tqven xart '  + age + ' wlis ' + ',' +  " shedit pirvel darbazshi "   )
+
+//         }
+//         else{
+//             console.log(  ' tqven xart '  + age + ' wlis '+' , ' +  " shedit meore darbazshi " )
+//     }
+    
+// }
+
+
+
+// else {
+//     console.log(
+//         'shesvla akrdzalulia'
+//     )
+// }
+
+
+
+//  var name = ["nika","nika2","nika3","nika4"]
+// // var demo= ["nika","nika2","nika3","nika4"]; 
+// console.log(name) ;
+// console.log(name[1]); 
+// console.log(name.length)
+
+// var namea = ["nika","nika2","nika3","nika4"]
+
+// // for (var i = 0; i < namea.length; i++) {
+// //  console.log( name[i])
+// // }
+// console.log(namea[3]);
+
+
+// var saxeli=[ "nika1", "nika2", "nika3", "nika4", "nika5", "nika6", ]
+// var age=[18,14,32,19,17,23]
+
+// for(i=0; i<saxeli.length; i++)
+// if(age[i] > 17      )
+// {
+//     console.log(saxeli[i] + ' aris ' + age[i] + ' wlis, ' + 'shesvla nebadartulia')
+// }
+
+// else{
+//     console.log(saxeli[i] + ' aris ' + age[i] + ' wlis, ' + 'shesvla akrdzalulia')
+// }
+
+
+// var username = prompt ( 'what is your name' )
+// alert (' nice to meet you ' + username)
+// console.log( 'also great to meet you ' + username )
+
+
+// var count = 0
+// while ( count < 20 )
+// console.log( count+=2 );
+
+// var str = 'hello'
+// var count = 0;
+//     console.log( str[count] );
+//     count++;
+
+// }
+
+// var ricxvi = 100
+// while ( ricxvi > 5  ){
+
+//     console.log ( ricxvi -=5 )
+// }
+
+// for( var count = 0; count <= 10 ; count+=2){
+//     console.log( count )
+// }
+
+// for( var i = -10; i < 20 ; i ++){
+//     console.log(i)
+// }
+
+// for( var i = 11; i <= 40; i +=3){
+
+//     if( i%5==0 && i%7==0 ){
+//         console.log(i)
+//     }
+    
+// }
+
+
+// for( i = 13; i <=50 ; i+=1){
+//     if ( i % 2 == 0 && i > 23 && i%5==0){
+//         console.log(i)
+//     }
+// }
+
+// for( i = 300; i <= 333; i++){
+//     if( i % 2 !== 0 && i % 5 ==0){
+//         console.log(i)
+//     }
+// }
+
+// function names(){
+//     document.write('tom, john, tom ,john')
+//     document.write('tom, john, tom ,john')
+//     document.write('tom, john, tom ,john')
+//     document.write('tom, john, tom ,john')
+// }
+
+
+// function names ( age, name )
+
+// {
+//     birthdate = 2019 - age 
+//     console.log( 'your name is ' + name + ' you are ' + birthdate )
+// }
+
+// names(24, 'nika')
+
+
+
+
+
+
+
+
+// var date1 = [ 19, 2005, 19, 27, 14, 85]
+
+
+// var date2 = [ 1998, 2005, 1995, 2007, 1994, 1985]
+
+// var date3 = [ 1, 5, 9, 7, 4, 5]
+
+
+
+// function printArray( qwe ){
+    
+
+// for (i= 0; i < qwe.length; i++){
+//     console.log(qwe[i])
+// }
+// }
+
+// printArray(date1)
+// console.log('--------')
+// printArray(date2)
+
+// function saxeli( person1, person2, person3){
+//     console.log('hi ' + person1 )
+//     console.log('hi ' + person2 )
+//     console.log('hi ' + person3)
+// }
+// saxeli('harry', 'ron', 'harmione')
+
+
+// var age = prompt('your age')
+// if(age < 18 ){
+//     console.log('mozardi')
+    
+// }
+//   else if (age < 60){
+//     console.log('beberi')
+// }
+
+
+// function jami ( ricxvi1, ricxvi2){
+//     console.log( ricxvi1 + ricxvi2 )
+// }
+// jami ( 6, 8 )
+
+
+
+
+// var i = 1
+// var sum = 0
+// while( i < 8){
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum)
+
+
+// function square(x) {
+//     return(x*x)
+// }
+
+// square(4)
+
+
+
+
+
+//  var  k = setInterval(() => {
+
+
+//  var h1 = document.querySelector('h1');
+  
+  
+//     var random =Math.floor(Math.random() * 100) 
+    
+    
+    
+//     console.log(random)
+       
+
+// }, 1000);
+
+
+// setTimeout(() => {
+   
+
+// }, 1000);
+
+// var body = document.querySelector('body')
+
+// function number ( x ){
+//     if ( x < 15 ){ 
+//         body.style.background = 'red'
+//     }
+//     else{
+//         body.style.background = 'green'
+
+//     }
+// }
+// number(10)
+
+// var body = document.querySelector ('body')
+
+// function name ( x) {
+//     if( x.length < 4 ){
+//         body.style.background = 'blue'
+//     }
+//     else{
+//         body.style.background = 'green'
+//     }
+// }
+
+// name ('aasdcasda')
+
+
+
+ 
+// var k = setInterval( () => {
+//     var random =Math.floor(Math.random() * 100) 
+//     console.log(random)
+//     if (random == 15){
+//         clearInterval(k)
+//     }
+// }, 100);
+
+
+ 
+
+//  var asda = document.getElementsByClassName('main_title')[0].
+//  style.backgroundColor = "red"
+
+
+
+// var x = document.getElementsByClassName("example");
+// var i;
+// for (i = 0; i < x.length; i++) {
+//   x[i].style.backgroundColor = "red";
+// }
+
+// var h1 = document.querySelector('h1')
+// h1.style.color = 'blue' 
+// h1.style.margin = '100px'
+// h1.style.border = '2px solid red'
+
+
+
+
+// var div = document.getElementsByClassName ('rame')
+// for( i = 0 ; i < div.length; i ++){
+
+//     div[i].style.color = 'red'
+// }
+
+
+
+// setInterval(() => {
+//     var c = Math.random()*100
+// var v = Math.random()*100
+// var h1 = document.getElementsByClassName('main_title')[0]
+// h1.style.left = c + '%';
+// h1.style.top = v + '%';
+// }, 1000);
+
+// var link_1 = document.querySelector( 'a');
+// link_1.getAttribute('href');
+// link_1.setAttribute('href' , 'www.dogs.com')
+// console.log(link_1.getAttribute('href'))
+
+// var button =document.querySelector ('button');
+// var paragraph = document.querySelector ('p');
+
+// button.addEventListener('click', function(){
+//     paragraph.textContent = 'someone cklicked me'
+// })
+
+
+
+
+// var name = 'john'
+// function showmessage() {
+//     var message = ('i am a javascript')
+//     alert('hello 
+//     ' +  name)
+// }
+
+// showmessage()
+
+
+// function sayhi(){
+//     alert('hello')
+// }
+
+// var func = sayhi
+
+// sayhi()
+// func()
+
+// var age = prompt('what is your age',15)
+// if (age <18 ){
+//     function wellcome(){
+//         alert('hello')
+//     }
+// }
+
+// else {
+//     function wellcome(){
+//         alert('great')
+//     }
+// }
+
+// wellcome()
+
+
+
+
+// function dosomething(){
+//     document.write('hello')
+// }
+
+// dosomething()
+
+// function sayhello(name) {
+//     console.log('hello there ' + name + '!')
+// }
+// sayhello("rusty")
+
+
+// function square(x){
+//     return x * x;
+// }
+
+// var result = square(40)
+
+
+// function test( x ){
+//     return x * 2;
+//     console.log(x);
+//     return(x / 2);
+// }
+
+// function isEven(num){
+//     if(num%2 === 0 ){
+//         return true;
+//     }
+//             else{
+//                 return false
+//     }
+// }
+
+// function factorial(num){
+//     var result = 1;
+//     for( var i =1; i <= num; i++ ){
+//         result = result * i;
+//     }
+//     return result;
+// }
+
+
+// setInterval(function() {
+//     document.write('i an nika <br> ')
+    
+// }, 1000);
+
+
+
+
+
+
+
+// document.querySelector('h1').style.color = 'red'
+
+// var body = document.querySelector('body');
+// var isBlue = false;
+
+// setInterval(function(){
+//     if (isBlue) {
+//         body.style.background = 'green'
+//     }else{
+//         body.style.background = 'grey'
+//     }
+//     isBlue = ! isBlue;
+// }, 3000);
+
+
+// var h1 = document.querySelector('h1');
+// var isRed = false;
+// setInterval(function(){
+//     if (isRed) {
+//         h1.style.color = 'red'
+//     }else{
+//         h1.style.color = 'blue'
+//     }
+//     isRed = !isRed;
+// },
+// 2000
+// )
+
+
+// var h2 = document.querySelector('h2');
+// var isYellow = false;
+
+// setInterval(function () {
+//     if(isYellow){
+//         h2.style.color = 'yellow'
+//     }
+//     else{
+//         h2.style.color = 'green'
+//     }
+
+//     isYellow = !isYellow
+// }, 1000)
+
+
+// var h3 = document.querySelector('h3');
+// var isRed = false;
+// setInterval(function(){
+//     if(isRed){
+//         h3.style.color = 'orange'
+//     }else{
+//         h3.style.color = 'black'
+//     }
+//     isRed = !isRed
+// },1000
+
+// )
+
+//  var ball = document.getElementById('ball')
+
+// // ball.style.display = 'block'
+
+
+
+//  var index = 0;
+//  document.getElementById('butt').addEventListener('click',function(){
+
+//     // if(index == 0){
+//     //     ball.style.display = 'block';
+
+//     //     index = 1;
+//     // }else {
+//     //     ball.style.display = 'none';
+
+//     //     index = 0;
+//     // }
+
+
+//     ball.classList.toggle("active");
+    
+// })
+
+
+// var h1 = document.querySelector('h1')
+// h1.addEventListener('click', function(){
+//     h1.style.color = 'red'
+// })
+
+
+
+
+// var btn = document.querySelector('button')
+// var p = document.querySelector('p')
+
+
+// btn.addEventListener('click', changetext)
+
+// function changetext(){
+//     p.textContent = 'someone clicked me'
+// }
+
+
+// var h1 = document.querySelector( 'h1')
+// h1.addEventListener('click',  function(){
+//     h1.textContent = 'text'
+// })
+
+
+// var h2 = document.querySelector('h2')
+// h2.addEventListener('click', function(){
+//     h2.style.backgroundColor = 'red'
+// }) 
+
+
+// var btn = document.querySelector('button');
+
+// btn.addEventListener('click', function(){
+//     document.body.style.backgroundColor = 'green' 
+// })
+
+
+
+// var button = document.querySelector('button')
+// var color = false
+
+// button.addEventListener('click', function(){
+//     if(color){
+//         document.body.style.backgroundColor = 'red'
+//         color = false
+//     }
+//     else{
+//         document.body.style.backgroundColor = 'green'
+//         color = true
+//     }
+// })
+
+
+// var button1 = document.getElementsByClassName('btn1')[0]
+// var button2 = document.getElementsByClassName('btn2')[0]
+// var reset = document.getElementsByClassName('reset')[0]
+
+// var score1 = 1
+// var score2 = 1
+
+// var player1 = document.getElementsByClassName('player1')[0]
+// var player2 = document.getElementsByClassName('player2')[0]
+
+// var wscore 
+
+
+// button1.addEventListener('click', function(){
+   
+//     if( score2 < wscore){
+//          player1.innerHTML = score1;
+//         if( score1 < wscore){
+        
+//              score1++;
+//         }
+//     }
+// })
+
+// button2.addEventListener('click', function(){
+    
+//     if( score1 < wscore){
+// player2.innerHTML = score2;
+//         if( score2 < wscore){
+//             score2++;
+//         }
+
+//     }
+// })
+
+// document.getElementById('wscore').addEventListener('keyup',function(){
+
+//      wscore = document.getElementById('wscore').value
+//     document.getElementsByTagName('p')[0].innerHTML = 'playing to: ' + wscore
+   
+// })
+
+// reset.addEventListener('click', function(){
+//    player1.innerHTML  = 0
+//    player2.innerHTML  = 0
+//    document.getElementsByTagName('p')[0].innerHTML = 'playing to: ' + 0
+//    document.getElementById('wscore').value = 0
+
+   
+//     score1 = 0
+//     score2 = 0
+//     wscore =0
+   
+   
+// })
+
+// var square = document.getElementsByClassName('square');
+//  for(i = 0; i < square.length; i ++ ){
+
+//      square[i].addEventListener('click', function(){
+//          this.style.background = 'red'
+//      })
+
+//  }
+
+
+
+// $('button').click(function(){
+//     $(this).css('background', 'green')
+
+// })
+
+// $('h1').click(function(){
+//     alert('you clicked h1')
+//     this.style.background = 'red'
+// })
+
+
+$('input').keypress(function(){
+    console.log('you pressed a key');
+    
+})
+
+
+// var a = 5; 
+// var b = 10; 
+// console.log(a, b)
+
+// var z = a;
+// var a = b;
+// var b = z
+// console.log( a, b)
+
+
+// $('button').click(function(){
+//     var text = $(this).text();
+//     alert('you clicked ' + text)
+//     $(this).css('background', 'red')
+// })
+
+// $('button').on('mouseenter',function(){
+//     $(this).css('font-weight', 'bold'),
+//     $(this).css('font-size','18px')
+// })
+
+// $('button').on('mouseout',function(){
+//     $(this).css('font-weight', 'normal');
+//     $(this).css('font-size', '15px')
+// })
+    
+
+// $('button').on('click',function(){
+//     $('div.square').fadeOut(2000, function(){
+//         console.log('complated');
+    
+//     });
+//     $('div.container').css('background', 'lightgrey')
+
+// })
+
+$('button').on('click',function(){
+    $('div.square').slideToggle(1500);
+   
+})
+
+
+$('h1').on('click',function(){
+    $(this).text( '52527272727')
+
+})
+
+$('h2').on('click',function(){
+    $(this).text('To Do List')
+
+})
+
+$('ul').on('click', 'li',function(){
+    $(this).toggleClass('complated')
+})
+
+$('ul').on('click','span', function(){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove()
+    });  
+    event.stopPropagation()
+})
+
+ $("input[type='text'").keypress(function(event){
+     if(event.which === 13){
+         var todoText =  $(this).val();
+         $(this).val("");
+         $('ul').append('<li><span>X</span> ' + todoText + '</li>')
+     }
+ })
+
+
+
+//  var number = 1 + Math.floor(Math.random() * 6);
+//  console.log(
+//      number
+//  )
+
+
+var qwe = ['img1.jpg','img2.jpg']
+var pl1_score = 1
+var pl2_score = 1
+
+
+ $('.game_button').on('click',function(){
+     
+     
+     var random = Math.floor(Math.random() * 2 )
+     console.log(random)
+     $('.image').attr('src', qwe[random])
+     
+    
+     if( random == 0){
+        $('.pl1').text("nika " + pl1_score)
+        pl1_score++
+     }
+
+     if( random == 1){
+         $('.pl2').text('tornike ' + pl2_score)
+         pl2_score ++
+     }
+    
+    })
+
+
